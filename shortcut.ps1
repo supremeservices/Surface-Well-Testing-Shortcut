58d241563ad8c7d2dd3aa4b634a795c49959e06c
supremeservices <158108630+supremeservices@users.noreply.github.com>
2024-08-15 10:33:36 -0500

    Update shortcut.ps1

--- shortcut.ps1
+++ shortcut.ps1
@@ -1,12 +1,19 @@
 
-$shortcutPath = "C:\Users\Public\Desktop\Surface Well Testing DVR.url"
-$targetUrl = "http://10.8.4.5:81"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$shortcut = $WScriptShell.CreateShortcut($shortcutPath)
-$shortcut.TargetPath = $targetUrl
-$shortcut.Save()
-if (Test-Path $shortcutPath) {
-    Write-Host "Shortcut created successfully at $shortcutPath"
-} else {
-    Write-Host "Failed to create shortcut at $shortcutPath"
+$shortcuts = @(
+    @{ Path = "C:\Users\cbuquet\Desktop\Houma Rental DVR.url"; Url = "http://10.8.4.2:81" },
+    @{ Path = "C:\Users\cbuquet\Desktop\Houma Frac DVR.url"; Url = "http://10.8.4.3:81" },
+    @{ Path = "C:\Users\cbuquet\Desktop\Surface Well Testing DVR.url"; Url = "http://10.8.4.5:81" }
+)
+
+foreach ($shortcut in $shortcuts) {
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $sc = $WScriptShell.CreateShortcut($shortcut.Path)
+    $sc.TargetPath = $shortcut.Url
+    $sc.Save()
+    
+    if (Test-Path $shortcut.Path) {
+        Write-Host "Shortcut created successfully at $($shortcut.Path)"
+    } else {
+        Write-Host "Failed to create shortcut at $($shortcut.Path)"
+    }
 }
